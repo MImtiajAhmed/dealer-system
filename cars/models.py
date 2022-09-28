b127205cc8594ad8977ef80aa59aad035496ff1c
Molla Imtiaj Ahmed <108191529+MImtiajAhmed@users.noreply.github.com>
2022-09-28 16:06:32 +0600

    added new app and model

---
+++ cars/models.py
@@ -0,0 +1,60 @@
+from turtle import color
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Car(models.Model):
+    
+    # State name listing
+    state_choice = (
+        ('NE', 'Nevada'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+    )
+    
+    # Year choice list
+    year_choice =[]
+    for r in range(2000, (datetime.now().year+1)):
+        year_choice.append((r,r))
+        
+    # Feature choice list
+    
+    feature_choices = (
+        ('NE', 'Nevada'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+        ('XX', 'XXXXXX'),
+    )
+    
+    prod_title = models.CharField(max_length=150)
+    city = models.CharField(choices = state_choice, max_length=100)
+    state = models.CharField(choices = state_choice, max_length=100)
+    color = models.CharField(max_length=100)
+    prod_model = models.CharField(max_length=100)
+    year = models.IntegerField('year', choices = year_choice)
+    prod_condition = models.CharField(max_length=100)
+    prod_price = models.IntegerField()
+    prod_description = models.TextField(max_length=200)
+    photo = models.ImageField(upload_to = 'photos/%Y/%m/%d', blank = True)
+    photo1 = models.ImageField(upload_to = 'photos/%Y/%m/%d', blank = True)
+    photo2 = models.ImageField(upload_to = 'photos/%Y/%m/%d', blank = True)
+    photo3 = models.ImageField(upload_to = 'photos/%Y/%m/%d', blank = True)
+    photo4 = models.ImageField(upload_to = 'photos/%Y/%m/%d', blank = True)
+    prod_features = models.CharField(choices=feature_choices, max_length=100)
+    prod_style = models.CharField(choices=feature_choices, max_length=100)
+    is_featured = models.BooleanField(max_length=100)
+    # You can add many more
+    added_date = models.DateTimeField(auto_now_add=True)
--- pages/admin.py
+++ pages/admin.py
@@ -4,14 +4,16 @@ from django.contrib import admin
 from .models import Team
 # from django.utils.html import format_html
 # not working thumbnail
-
-class TeamAdmin(admin.ModelAdmin):
-    # not working thumbnail
+# not working thumbnail
     # def thumbnail(self, objects):
     #     return format_html('< img src="{}" width = "40" >'.format(objects.photo.url))
     
     # thumbnail.short_description = 'Photo'
     # not working thumbnail
+    
+
+class TeamAdmin(admin.ModelAdmin):
+    
     list_display = ('id', 'first_name', 'last_name', 'designation', 'created_date')
     list_display_links = ('first_name', )
     search_fields = ('first_name', 'designation')
